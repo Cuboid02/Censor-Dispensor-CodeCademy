6fcbe140e532117837509d93240cfe566673cb95
Cuboid02 <williamqtran@hotmail.co.uk>
2020-12-06 15:28:44 +0000

    Parts 1 and 2 complete

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -9,15 +9,54 @@ email_four = open("email_four.txt", "r").read()
 # Mr. Cloudy has asked you to use the function to censor all instances of the phrase learning algorithms from the first email, email_one.
 # Mr. Cloudy doesn’t care how you censor it, he just wants it done.
 
+def censor_word(text, word):
+    block = ""
+    for n in range(0, len(word)):
+        if word[n] == " ":
+            block += " "
+        else:
+            block += "*"
+    return text.replace(word, block)
+
+# print(censor_word(email_one, "learning algorithm"))
 
 
 # 2. Write a function that can censor not just a specific word or phrase from a body of text, but a whole list of words and phrases,
 # and then return the text.
 # Mr. Cloudy has asked that you censor all words and phrases from the following list in email_two.
 
-# proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+proprietary_terms = ["personality matrix", "sense of self", "self-preservation", "learning algorithm", "herself", "her", "she"]
 
+def censor_list(text, list_of_words):
+    for word in list_of_words:
+        print("Now replacing {0}".format(word))
+        block = ""
+        for n in range(0, len(word)):
+            if word[n] == " ":
+                block += " "
+            else:
+                block += "*"
+#        text = text.replace(word, block)
+        text_lowered = text.lower()
+        word_lowered = word.lower()
+        context = [idx for idx, char in enumerate(text_lowered) if char == word_lowered[0]]
+        for i in context:
+            capture = text_lowered[i : i+len(word_lowered)]
+#            print(capture)
+            if capture == word_lowered:# and text[i-1] == " " and text[i+len(word)+1] == " ":
+#                char_before = text[i-1]
+#                print(char_before)
+#                if char_before == " " or char_before == "\n":
+#                    char_after = text[i+len(word)]
+#                    print(char_after)
+#                    if char_after == " " or char_after == "\n" or char_after == "s":
+                capture = text[i : i+len(word)]
+#                        print(capture)
+#                        print("Char_before : '{0}' and char_after : '{1}'".format(char_before, char_after))
+                text = text.replace(capture, block)
+    return text
 
+print(censor_list(email_two, proprietary_terms))
 
 # 3. The most recent email update has concerned Mr. Cloudy, but not for the reasons you might think.
 # He tells you, “this is too alarmist for the Board of Investors! Let’s tone down the negative language and
